802e0e2e26c96e7f9f52c4423c893562c581246b
sh4869 <nobuk4869@gmail.com>
2016-08-09 20:29:29 +0900

    fix Scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,7 @@ var path = require('path');
 const GH_TOKEN = process.env.GH_TOKEN;
 
 ghpages.publish(path.join(__dirname, '..','public'),{
-  repo: 'https://${GH_TOKEN}@github.com/sh4869/gh-pages-travis-test',
+  repo: 'https://' + GH_TOKEN + '@github.com/sh4869/gh-pages-travis-test',
   user: {
     name: 'sh4869',
     email: 'nobuk4869@gmail.com'
